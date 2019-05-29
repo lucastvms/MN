687a90bc72b142bb738fcbd6de52e1eb9b9f9ae8
Lucas Vieira <lucas.tvms@hotmail.com>
2019-05-29 18:59:46 -0300

    Create matlab1.m

---
+++ matlab1.m
@@ -0,0 +1,18 @@
+a = 10
+b = 2.5*10^23
+c = 2+ 3*i
+e = exp(1)
+d = e^(j*2*pi/3)
+
+aVec = [3.14 15 9 26]
+bVec = [2.71; 8; 28; 182]
+cVec = 5:-0.2:-5
+dVec = logspace(0, 1, 100)
+eVec = 'Hello'
+
+aMat = ones(9)*2
+
+
+
+% http://w3.impa.br/~zubelli/tutorial/node3.html
+% https://www.mathworks.com/help/matlab/ref/logspace.html
